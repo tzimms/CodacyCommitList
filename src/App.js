1820cdb15dcabd102dfa43753ab11f8b53369c6f
Toria <tzimms7@gmail.com>
2020-03-25 17:53:32 +0000

    linting

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import axios from 'axios';
 import Posts from './components/Posts' ;
 import Pagination from './components/Pagination';
 
-
-
 const App = () => {
   const [commitListData, setCommitList] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -28,49 +26,42 @@ const App = () => {
     fetch('./commitList.txt')
     .then(response => response.text())
     .then(data => { 
-      let trim = "[" + data.replace(/(^,)|(,$)/g, "") +"]"
-      let newData = JSON.parse(trim) 
+      let trim = "[" + data.replace(/(^,)|(,$)/g, "") +"]";
+      let newData = JSON.parse(trim);
       setBackupdata(newData)
       setLoading(false)
     })
   }
 
+
   const getCurrentPost = () => {
     if (commitListData.length > 10) {
       return commitListData.slice(indexOfFirstPost, indexOfLastPost);
     } else {
-      return backupData.slice(indexOfFirstPost, indexOfLastPost)
+      return backupData.slice(indexOfFirstPost, indexOfLastPost);
     }
-  }
-
-  const getTotalPosts = () => {
-    if (commitListData.length > 10) {
-      return commitListData.length
-    } else {
-      return backupData.length
-    }
-
   }
 
   
   const indexOfLastPost = currentPage * postsPerPage; 
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = getCurrentPost();
-  const totalPosts = getTotalPosts();
+  const totalPosts = commitListData.length > 10 ? commitListData.length : backupData.length;
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  
   return (
     <div className="container mt-5">
       <h1 className='text-primary mb-3'>List of Github Commits</h1>
       < Pagination 
-        postsPerPage = {postsPerPage}
-        totalPosts = {totalPosts}
-        paginate = {paginate}
+          postsPerPage = {postsPerPage}
+          totalPosts = {totalPosts}
+          paginate = {paginate}
        />
       < Posts
-        commitList = {currentPosts}
-        loading = {loading}
-        placeholder = {getText}
-        backupData = {currentPosts}
+          commitList = {currentPosts}
+          loading = {loading}
+          placeholder = {getText}
+          backupData = {currentPosts}
         /> 
        
     </div>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
     const pageNumbers = [];
-
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++){
         pageNumbers.push(i);
     }
