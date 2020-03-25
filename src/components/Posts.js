dd306f2c1997d72971bb986c73d5b96d94582bd8
Toria <tzimms7@gmail.com>
2020-03-25 15:05:16 +0000

    pagination and display of backup data

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,33 @@
 import React from 'react'
 
-const Posts = ({ commitList, loading, placeholder }) => {
+const Posts = ({ commitList, loading, placeholder, backupData }) => {
     if (loading) {
         return (
         <button onClick={ () => placeholder() }> Redirect to Local </button>
         )
-    
     }
+    else if (commitList.length > 1){
     return (
     <ul className="list-group mb-4">
-    {commitList.map((com) => (
+    {
+        commitList.map((com) => (
           <li key={com.sha} className='list-group-item'>
           {com.commit.author.name} committed on {com.commit.author.date}
           </li>
       ))
       } </ul>
+    )}
+    return (
+        <ul className="list-group mb-4">
+    {
+        backupData.map((com) => (
+          <li key={com.date} className='list-group-item'>
+          {com.author} committed on {com.date}
+          </li>
+      ))
+      } </ul>
     )
+
 }
 
 
